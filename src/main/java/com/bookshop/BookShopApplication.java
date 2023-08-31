430a9195f988b1b831b14706195749cb79052cc7
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-09-01 01:58:15 +0300

    created a "clear" base

--- src/main/java/com/bookshop/BookShopApplication.java
+++ src/main/java/com/bookshop/BookShopApplication.java
@@ -1,39 +1,12 @@
 package com.bookshop;
 
-import com.bookshop.model.Book;
-import com.bookshop.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookShopApplication {
-    private final BookService bookService;
-
-    @Autowired
-    public BookShopApplication(BookService bookService) {
-        this.bookService = bookService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookShopApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book colourOfMagic = new Book();
-            colourOfMagic.setAuthor("Sir Terence David John «Terry» Pratchett");
-            colourOfMagic.setTitle("The Colour of Magic");
-            colourOfMagic.setIsbn("0062225677");
-            colourOfMagic.setPrice(BigDecimal.valueOf(4.99));
-
-            bookService.save(colourOfMagic);
-
-            System.out.println(bookService.findAll());
-        };
     }
 }
