1126c666179516f1e283e266e1214599c65312d4
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-09-05 12:19:35 +0300

    removed redundant "value" in @RequestMapping of BookController, removed redundant CreateBookRequestDto from BookShopApplication, removed redundant empty line in pom.xml, made CreateBookRequestDto a record class

--- src/main/java/com/bookshop/BookShopApplication.java
+++ src/main/java/com/bookshop/BookShopApplication.java
@@ -1,11 +1,9 @@
 package com.bookshop;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class BookShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookShopApplication.class, args);
