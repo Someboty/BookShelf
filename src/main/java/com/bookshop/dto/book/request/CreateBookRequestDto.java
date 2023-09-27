9dbf27474da7c91dc17018512e804bcf2db6e199
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-09-27 04:21:42 +0300

    Added tests for Category and Book

--- src/main/java/com/bookshop/dto/book/request/CreateBookRequestDto.java
+++ src/main/java/com/bookshop/dto/book/request/CreateBookRequestDto.java
@@ -17,19 +17,19 @@ public class CreateBookRequestDto {
     @Schema(example = "title")
     private String title;
 
-    @NotNull(message = "author cannot be null, should be set")
+    @NotNull(message = "author can't be null, should be set")
     @Size(min = 1, max = 255, message = "author should be between 1 and 255 characters")
     @Schema(example = "author")
     private String author;
 
     @Isbn
-    @NotNull(message = "ISBN cannot be null, should be set")
+    @NotNull(message = "ISBN can't be null, should be set")
     @Size(min = 10, max = 17, message = "ISBN should be between 10 and 17 characters")
     @Schema(example = "978-3-16-148410-0")
     private String isbn;
 
-    @NotNull(message = "price cannot be null, should be set")
-    @Min(value = 0, message = "price cannot be less than 0")
+    @NotNull(message = "price can't be null, should be set")
+    @Min(value = 0, message = "price can't be less than 0")
     @Schema(example = "12.99")
     private BigDecimal price;
 
