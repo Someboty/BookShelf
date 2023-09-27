cea954ac662fa86ac7e52c8b7c0ec5e811ab001b
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-09-27 12:59:15 +0300

    Added tests for Category and Book

---
+++ src/test/resources/database/books/add-one-category.sql
@@ -0,0 +1,1 @@
+insert into categories(id, name, description, is_deleted) values (1, 'drama', 'some sad stuff', false);
---
+++ src/test/resources/database/books/add-three-categories.sql
@@ -0,0 +1,3 @@
+insert into categories(id, name, description, is_deleted) values (1, 'science', 'some clever stuff', false);
+insert into categories(id, name, description, is_deleted) values (2, 'biography', 'about Person', false);
+insert into categories(id, name, description, is_deleted) values (3, 'comedy', 'things that make you smile', false);
